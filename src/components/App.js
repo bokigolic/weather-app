8ceb98b684b5fef97a68c121ca77f66c7baaaa02
Bojan Golic <bokigolic32@gmail.com>
2022-03-28 17:11:00 -0400

    added search form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,35 @@
+import { useState } from 'react';
+
 const App = () => {
+
+  const preset = {
+    search: ''
+  };
+
+  const [formState, setFormState] = useState(preset);
+
+  const handleChange = (e) => {
+    // univerzalni handler za sva input polja
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
   return (
-    <div> App </div>
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        name="search"
+        value={formState.search}
+        onChange={handleChange}
+      />
+
+    </div>
   );
 };
 
