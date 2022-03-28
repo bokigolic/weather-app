8aba630f1108849b900b356f083d1eecdabf7298
Bojan Golic <bokigolic32@gmail.com>
2022-03-28 16:59:52 -0400

    App started, redux works

---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    nesto: 'bla bla'
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'NEKI_ACTION ':
+
+            break;
+
+        default:
+            return state;
+    }
+
+};
+
+
+export default rootReducer;
