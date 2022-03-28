9e0d0738c655228a27e8e12d08294837fb81a1c7
Bojan Golic <bokigolic32@gmail.com>
2022-03-28 18:07:48 -0400

    displaying forecast result

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from 'react';
+import { resultingClientExists } from 'workbox-core/_private';
 import { ajax } from '../utils/ajax-adapter';
 
 const App = () => {
+  const [result, setResult] = useState({});
+
+  let resultReady = false;
+  if (result && result.id) {
+    resultReady = true;
+  }
 
   const preset = {
     search: ''
@@ -28,15 +35,34 @@ const App = () => {
     if (q !== '') {
       console.log('aktiviramo hot serch za reči: ', q);
       ajax.getWeatherSearch(q)
-      .then(obradjeni_response => {
-        if (obradjeni_response) {
-          // sigurno uspeo response jer neusepli stize kao false
-          console.log('usepo response :)', obradjeni_response);
-        }
-      })
+        .then(obradjeni_response => {
+          if (obradjeni_response) {
+            // sigurno uspeo response jer neusepli stize kao false
+            console.log('usepo response :)', obradjeni_response);
+            setResult(obradjeni_response);
+          }
+        })
     }
-    
+
   }, [q]);
+
+  let jsxResult = (
+    <div>No results yet</div>
+  );
+  if (resultReady) {
+    // ako su rezultati spremni ispisujemo ih
+    jsxResult = (
+      <div>
+        <div>ID: {result.id}</div>
+        <div>Name: {result.name}</div>
+        <div>icon: {result.weather[0].main}</div>
+        <div>description: {result.weather[0].description}</div>
+        <div>Temperature: {result.main.temp} degree</div>
+        <div>Wind speed: {result.wind.speed}</div>
+      </div>
+    );
+  }
+
 
   return (
     <div>
@@ -47,6 +73,9 @@ const App = () => {
         value={formState.search}
         onChange={handleChange}
       />
+
+      <h3>Forecast results</h3>
+      {jsxResult}
 
     </div>
   );
